0958fdd12b502dc1bd2d9ed95cd6482137947527
Niketan2001 <72155531+Niketan2001@users.noreply.github.com>
2023-02-08 14:22:55 +0530

    Updates

---
+++ precache-manifest.1b0e4bdd635300935161b0e824dd3de4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5657e99acb0a6cf81e6",
+    "url": "/Manipulating-Text/static/css/main.e24284a6.chunk.css"
+  },
+  {
+    "revision": "d5657e99acb0a6cf81e6",
+    "url": "/Manipulating-Text/static/js/main.d5657e99.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/Manipulating-Text/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/Manipulating-Text/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "e4cffd4deb015fed125b",
+    "url": "/Manipulating-Text/static/js/runtime~main.e4cffd4d.js"
+  },
+  {
+    "revision": "aa08fe33b5437803d28e291c0bc5addb",
+    "url": "/Manipulating-Text/index.html"
+  }
+];
